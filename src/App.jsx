17f820f5c6785d2c7c0c529525ea633a531f3474
khasanov_ibroxim <xibroxim11@gmail.com>
2025-02-16 19:20:23 +0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePageFootball from "./pages/Home_Page_Football/homePageFootball.jsx";
 import {RouterFootballData} from "./utils/consts.jsx";
 import App_bar_football from "./component/App_bar/app_bar_football.jsx";
+import LoaderFootball from "./component/loader/loader_football.jsx";
 
 
 function App(props) {
+    const [isLoading, setIsLoading] = useState(true);
+    const tg = window.Telegram.WebApp;
+
+    useEffect(() => {
+        // 2 soniyadan keyin loaderni o'chirish
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 200);
+
+        tg.expand();
+        tg.headerColor = "#378805";
+        tg.bottomBarColor = "#378805";
+        tg.isVerticalSwipesEnabled = false;
+        tg.isHorizontalSwipesEnabled = false;
+
+        return () => clearTimeout(timer);
+    }, [tg]);
+
+    if (isLoading) {
+        return <>
+            <LoaderFootball />
+        </>;
+    }
+
     return (
         <BrowserRouter>
             <Routes>
